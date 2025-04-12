f9778c225b2b007bc9c80d8d88fcb8a1fb670e51
AlanHuang <alan0988469026@gmail.com>
2025-04-12 19:28:43 +0800

    Fix the new nvidia docker

--- Package/NVIDIA/install-nv-docker.sh
+++ Package/NVIDIA/install-nv-docker.sh
@@ -18,6 +18,7 @@ curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-contai
 sudo apt-get update
 sudo apt-get install -y nvidia-container-toolkit
 
+# NOTE: this line break the new version nvidia-container-toolkit
 # Change the user who execute nvidia-container-toolkit
 # Reference: https://askubuntu.com/a/1319123
-sudo sed -i 's/^#user.*/user = "root:vglusers"/' /etc/nvidia-container-runtime/config.toml
+# sudo sed -i 's/^#user.*/user = "root:vglusers"/' /etc/nvidia-container-runtime/config.toml
